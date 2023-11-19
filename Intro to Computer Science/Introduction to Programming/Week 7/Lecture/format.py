8e2853adc1058b135bbdc90e088321c931047234
nate-levine <n8levine11@gmail.com>
2023-11-19 12:56:56 -0700

    Introduction to Programming - Week 7 - Lecture

---
+++ Intro to Computer Science/Introduction to Programming/Week 7/Lecture/format.py
@@ -0,0 +1,7 @@
+import re
+
+
+name = input("What's your name? ").strip()
+matches = re.search(r"^(.+), *(.+)$", name)
+if matches:
+    last, first = matches.groups()
---
+++ Intro to Computer Science/Introduction to Programming/Week 7/Lecture/validate.py
@@ -0,0 +1,10 @@
+import re
+
+
+email = input("What's your email? ").strip()
+
+
+if re.search(r"^\w+@(\w+\.)?edu$", email, re.IGNORECASE):
+    print("Valid")
+else:
+    print("Invalid")
