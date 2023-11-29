291cc22b7de1f8b669afabeb3dbc47b4e9d7691c
nate-levine <n8levine11@gmail.com>
2023-11-28 22:28:56 -0700

    Introduction to CS - Problem Set 0

---
+++ Intro to Computer Science/Introduction to Computer Science/Problem Sets/Problem Set 0/ps0.py
@@ -0,0 +1,10 @@
+import numpy
+
+
+#0-1
+x = int(input("Enter number x: "))
+y = int(input("Enter number y: "))
+x_exp_y = x**y
+print(f"{x}**{y} = {x_exp_y}")
+log_2_x = numpy.log2(x)
+print(f"log({x}) = {log_2_x}")
