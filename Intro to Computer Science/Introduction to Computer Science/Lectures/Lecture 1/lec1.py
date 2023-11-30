99f9ae4624c268010d0289336856d0ab1368d37e
nate-levine <n8levine11@gmail.com>
2023-11-29 21:24:16 -0700

    Introduction to CS - Lecture 3

---
+++ Intro to Computer Science/Introduction to Computer Science/Lectures/Lecture 1/lec1.py
@@ -0,0 +1,48 @@
+pi = 3.14159
+radius = 2.2
+# area of circle equation <- this is a comment
+area = pi*(radius**2)
+print(area)
+
+# change values of radius <- another comment
+# use comments to help others understand what you are doing in code
+# Add 1 to radius
+radius = radius + 1
+# Print the area
+print(area)     # area doesn't change
+# Find the area with the new radius
+area = pi*(radius**2)
+# Print the new area
+print(area)
+
+
+#############################
+#### COMMENTING LINES #######
+#############################
+# to comment MANY lines at a time, highlight all of them then CTRL+1
+# do CTRL+1 again to uncomment them
+# try it on the next few lines below!
+
+#area = pi*(radius**2)
+#print(area)
+#radius = radius + 1
+#area = pi*(radius**2)
+#print(area)
+
+#############################
+#### AUTOCOMPLETE #######
+#############################
+# Spyder can autocomplete names for you
+# start typing a variable name defined in your program and hit tab 
+# before you finish typing -- try it below
+
+# define a variable
+a_very_long_variable_name_dont_name_them_this_long_pls = 0
+
+# below, start typing a_ve then hit tab... cool, right!
+# use autocomplete to change the value of that variable to 1
+a_very_long_variable_name_dont_name_them_this_long_pls = 1
+
+# use autocomplete to write a line that prints the value of that long variable
+# notice that Spyder also automatically adds the closed parentheses for you!
+print(a_very_long_variable_name_dont_name_them_this_long_pls)
