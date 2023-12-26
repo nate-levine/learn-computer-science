35c4647c2d33eae5e5f49536a0ef657a42b72082
nate-levine <n8levine11@gmail.com>
2023-12-26 17:00:58 -0500

    Systematic Program Design - Lecture 2

---
+++ Core Computer Science/Systematic Program Design/Lectures/Lecture 2/course-name.rkt
@@ -0,0 +1,14 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname course-name) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;; CourseName is a String
+;; interp. the name of a course
+(define CN1 "Systematic Program Design")
+(define CN2 "Introduction to Programming with Python")
+
+#;
+define (fn-for-course-name)
+  (... cn))
+
+;; Template rules used:
+;;  - atomic non-distinct: String
